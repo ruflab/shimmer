bc953b5006d17581b5665a2de1190f18a4eb4e4e
bdvllrs <bdvllrs@gmail.com>
2023-07-06 14:42:09 +0200

    Add load_structured_config in init.

--- shimmer/__init__.py
+++ shimmer/__init__.py
@@ -1,4 +1,13 @@
-from shimmer.config import ShimmerInfoConfig, load_config
+from shimmer.config import (
+    ShimmerInfoConfig,
+    load_config,
+    load_structured_config,
+)
 from shimmer.version import __version__
 
-__all__ = ["__version__", "load_config", "ShimmerInfoConfig"]
+__all__ = [
+    "__version__",
+    "load_config",
+    "load_structured_config",
+    "ShimmerInfoConfig",
+]
