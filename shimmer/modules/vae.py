2188018eb9f57e8f8f81c1914c776dcab8ffb032
bdvllrs <bdvllrs@gmail.com>
2023-07-20 09:48:56 +0000

    Simplify VAE class to only accept Sequence of Tensors.

--- shimmer/modules/vae.py
+++ shimmer/modules/vae.py
@@ -26,11 +26,21 @@ def gaussian_nll(mu, log_sigma, x):
     )
 
 
+class VAEEncoder(nn.Module):
+    def forward(self, x: Sequence[torch.Tensor]) -> torch.Tensor:
+        ...
+
+
+class VAEDecoder(nn.Module):
+    def forward(self, x: torch.Tensor) -> Sequence[torch.Tensor]:
+        ...
+
+
 class VAE(nn.Module):
     def __init__(
         self,
-        encoder: nn.Module,
-        decoder: nn.Module,
+        encoder: VAEEncoder,
+        decoder: VAEDecoder,
         beta: float = 1,
     ):
         super().__init__()
@@ -42,36 +52,19 @@ class VAE(nn.Module):
         self.encoder = encoder
         self.decoder = decoder
 
-    def encode(self, x: torch.Tensor | Sequence[torch.Tensor]) -> torch.Tensor:
+    def encode(self, x: Sequence[torch.Tensor]) -> torch.Tensor:
         mean_z, _ = self.encoder(x)
         return mean_z
 
-    def decode(self, z: torch.Tensor) -> Sequence[torch.Tensor] | torch.Tensor:
+    def decode(self, z: torch.Tensor) -> Sequence[torch.Tensor]:
         return self.decoder(z)
 
     def forward(
-        self, x: torch.Tensor | Sequence[torch.Tensor]
-    ) -> tuple[
-        tuple[torch.Tensor, torch.Tensor],
-        torch.Tensor | Sequence[torch.Tensor],
-    ]:
+        self, x: Sequence[torch.Tensor]
+    ) -> tuple[tuple[torch.Tensor, torch.Tensor], Sequence[torch.Tensor],]:
         mean, logvar = self.encoder(x)
         z = reparameterize(mean, logvar)
 
         x_reconstructed = self.decoder(z)
 
         return (mean, logvar), x_reconstructed
-
-    def reconstruction_loss(
-        self, x_reconstructed: torch.Tensor, x: torch.Tensor
-    ) -> torch.Tensor:
-        return gaussian_nll(x_reconstructed, torch.tensor(0), x).sum()
-
-    def get_losses(
-        self, x: torch.Tensor
-    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        (mean, logvar), x_reconstructed = self(x)
-        reconstruction_loss = self.reconstruction_loss(x_reconstructed, x)
-        kl_divergence = kl_divergence_loss(mean, logvar)
-        total_loss = reconstruction_loss + self.beta * kl_divergence
-        return reconstruction_loss, kl_divergence, total_loss
