81782755eaac4fe852329146bd43b05639b4cc79
bdvllrs <bdvllrs@gmail.com>
2024-03-06 08:28:45 +0000

    Add TypeAlias annotation

--- shimmer/types.py
+++ shimmer/types.py
@@ -1,101 +1,68 @@
 from collections.abc import Mapping
-from typing import Any, Literal
+from typing import Any, Literal, TypeAlias
 
 import torch
 
-RawDomainGroupT = Mapping[str, Any]
+module_var = ""
+"""some doc"""
+
+RawDomainGroupT: TypeAlias = Mapping[str, Any]
 """
 Matched raw unimodal data from multiple domains.
 Keys of the mapping are domains names.
-
-```python
-    RawDomainGroupT = Mapping[str, Any]
-```
 """
 
-RawDomainGroupDT = dict[str, Any]
+RawDomainGroupDT: TypeAlias = dict[str, Any]
 """
 Matched raw unimodal data from multiple domains.
 Keys of the dict are domains names.
 
 This is a more specific version of `RawDomainGroupT` used in method's outputs.
-
-```python
-    RawDomainGroupDT = dict[str, Any]
-```
 """
 
-LatentsDomainGroupT = Mapping[str, torch.Tensor]
+LatentsDomainGroupT: TypeAlias = Mapping[str, torch.Tensor]
 """
 Matched unimodal latent representations from multiple domains.
 Keys of the mapping are domains names.
-
-```python
-    LatentsDomainGroupT = Mapping[str, torch.Tensor]
-```
 """
 
-LatentsDomainGroupDT = dict[str, torch.Tensor]
+LatentsDomainGroupDT: TypeAlias = dict[str, torch.Tensor]
 """
 Matched unimodal latent representations from multiple domains.
 Keys of the dict are domains names.
 
 This is a more specific version of `LatentsDomainGroupT` used in method's outputs.
-
-```python
-    LatentsDomainGroupDT = dict[str, torch.Tensor]
-```
 """
 
-LatentsDomainGroupsT = Mapping[frozenset[str], LatentsDomainGroupT]
+LatentsDomainGroupsT: TypeAlias = Mapping[frozenset[str], LatentsDomainGroupT]
 """
 Mapping of `LatentsDomainGroupT`. Keys are frozenset of domains matched in the group.
 Each group is independent and contains different data (unpaired).
-
-```python
-    LatentsDomainGroupsT = Mapping[frozenset[str], LatentsDomainGroupT]
-```
 """
 
-LatentsDomainGroupsDT = dict[frozenset[str], LatentsDomainGroupDT]
+LatentsDomainGroupsDT: TypeAlias = dict[frozenset[str], LatentsDomainGroupDT]
 """
 Mapping of `LatentsDomainGroupDT`.
 Keys are frozenset of domains matched in the group.
 Each group is independent and contains different data (unpaired).
 
 This is a more specific version of `LatentsDomainGroupsT` used in method's outputs.
-
-```python
-    LatentsDomainGroupsDT = dict[frozenset[str], LatentsDomainGroupDT]
-```
 """
 
-RawDomainGroupsT = Mapping[frozenset[str], RawDomainGroupT]
+RawDomainGroupsT: TypeAlias = Mapping[frozenset[str], RawDomainGroupT]
 """
 Mapping of `RawDomainGroupT`. Keys are frozenset of domains matched in the group.
 Each group is independent and contains different data (unpaired).
-
-```python
-    RawDomainGroupsT = Mapping[frozenset[str], RawDomainGroupT]
-```
 """
 
-RawDomainGroupsDT = dict[frozenset[str], RawDomainGroupDT]
+RawDomainGroupsDT: TypeAlias = dict[frozenset[str], RawDomainGroupDT]
 """
 Mapping of `RawDomainGroupT`. Keys are frozenset of domains matched in the group.
 Each group is independent and contains different data (unpaired).
 
 This is a more specific version of `RawDomainGroupsT` used in method's outputs.
-
-```python
-    RawDomainGroupsDT = dict[frozenset[str], RawDomainGroupDT]
-```
 """
 
-ModelModeT = Literal["train", "val", "test", "val/ood", "test/ood"]
+ModelModeT: TypeAlias = Literal["train", "val", "test", "val/ood", "test/ood"]
 """Mode used by pytorch lightning.
-
-```python
-    ModelModeT = Literal["train", "val", "test", "val/ood", "test/ood"]
-```
 """
