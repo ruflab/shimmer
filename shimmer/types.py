93ed1763ad92c3b1811102ab412e6c86533f8670
bdvllrs <bdvllrs@gmail.com>
2024-03-18 16:25:41 +0000

    Improve types docstring

--- shimmer/types.py
+++ shimmer/types.py
@@ -6,35 +6,186 @@ import torch
 RawDomainGroupT = Mapping[str, Any]
 """
 Matched raw unimodal data from multiple domains.
-Keys of the mapping are domains names.
+Keys of the mapping are domains names and values are the domain data.
+
+All values in the mapping should be matched and represent the same information.
+
+Example:
+    ```python
+    def fun(domain_group: RawDomainGroupT): ...
+
+
+    x = {
+        "vision": PIL.Image.Image("path/to/dog/picture.png"),
+        "language": "This is a picture of a dog.",
+    }
+
+    fun(x)
+    ```
+
+Note:
+    This type uses `collections.abc.Mapping` and is used for functions' inputs.
+    Use `RawDomainGroupDT` for functions' outputs.
+
+    This allows to be more generic and allow passing other mappings.
 """
 
 RawDomainGroupDT = dict[str, Any]
 """
+Output type version of `RawDomainGroupT`.
 Matched raw unimodal data from multiple domains.
-Keys of the dict are domains names.
+Keys of the mapping are domains names and values are the domain data.
 
-This is a more specific version of `RawDomainGroupT` used in method's outputs.
+Example:
+    ```python
+    def fun() -> RawDomainGroupDT: 
+        return {
+            "vision": PIL.Image.Image("path/to/dog/picture.png"),
+            "language": "This is a picture of a dog.",
+        }
+
+    ```
+
+Note:
+    This type uses `dict`s and is used for functions' outputs.
+    Use `RawDomainGroupT` for functions' inputs.
+
 """
 
 LatentsDomainGroupT = Mapping[str, torch.Tensor]
 """
 Matched unimodal latent representations from multiple domains.
-Keys of the mapping are domains names.
+Keys of the mapping are domains names and values are `torch.Tensor` latent 
+representation of the domain.
+
+Example:
+    ```python
+    def fun(domain_group: LatentsDomainGroupT): ...
+
+
+    x = {
+        "vision": torch.Tensor([0.0, 1.0, 0.0, ...]),
+        "language": torch.Tensor([0.0, 0.3, 0.2, ...]),
+    }
+
+    fun(x)
+    ```
+
+Note:
+    This type uses `collections.abc.Mapping` and is used for functions' inputs.
+    Use `LatentsDomainGroupDT` for functions' outputs.
+
+    This allows to be more generic and allow passing other mappings.
 """
 
 LatentsDomainGroupDT = dict[str, torch.Tensor]
 """
 Matched unimodal latent representations from multiple domains.
-Keys of the dict are domains names.
+Keys of the dict are domains names and values are `torch.Tensor` latent
+representation of the domain.
 
-This is a more specific version of `LatentsDomainGroupT` used in method's outputs.
+Example:
+    ```python
+    def fun() -> LatentsDomainGroupDT: 
+        return {
+            "vision": torch.Tensor([0.0, 1.0, 0.0, ...]),
+            "language": torch.Tensor([0.0, 0.3, 0.2, ...]),
+        }
+
+    ```
+
+Note:
+    This type uses `dict`s and is used for functions' outputs.
+    Use `LatentsDomainGroupT` for functions' inputs.
+"""
+
+RawDomainGroupsT = Mapping[frozenset[str], RawDomainGroupT]
+"""
+Mapping of `RawDomainGroupT`. Keys are frozenset of domains matched in the group.
+Each group is independent and contains different data (unpaired).
+
+Example:
+    ```python
+    def fun() -> RawDomainGroupsDT: 
+        return {
+            frozenset(["vision"]): {
+                "vision": PIL.Image.Image("path/to/cat/picture.png"),
+            },
+            frozenset(["language"]): {
+                "language": "This is a picture of a rabbit.",
+            },
+            frozenset(["vision", "language"]): {
+                "vision": PIL.Image.Image("path/to/dog/picture.png"),
+                "language": "This is a picture of a dog.",
+            },
+        }
+
+    ```
+
+Note:
+    This type uses `dict`s and is used for functions' outputs.
+    Use `RawDomainGroupsT` for functions' inputs.
+"""
+
+RawDomainGroupsDT = dict[frozenset[str], RawDomainGroupDT]
+"""
+Mapping of `RawDomainGroupT`. Keys are frozenset of domains matched in the group.
+Each group is independent and contains different data (unpaired).
+
+Example:
+    ```python
+    def fun() -> RawDomainGroupsDT: 
+        return {
+            frozenset(["vision"]): {
+                "vision": PIL.Image.Image("path/to/cat/picture.png"),
+            },
+            frozenset(["language"]): {
+                "language": "This is a picture of a rabbit.",
+            },
+            frozenset(["vision", "language"]): {
+                "vision": PIL.Image.Image("path/to/dog/picture.png"),
+                "language": "This is a picture of a dog.",
+            },
+        }
+
+    ```
+
+Note:
+    This type uses `dict`s and is used for functions' outputs.
+    Use `RawDomainGroupsT` for functions' inputs.
 """
 
 LatentsDomainGroupsT = Mapping[frozenset[str], LatentsDomainGroupT]
 """
 Mapping of `LatentsDomainGroupT`. Keys are frozenset of domains matched in the group.
 Each group is independent and contains different data (unpaired).
+
+Example:
+    ```python
+    def fun(domain_group: LatentsDomainGroupsT): ...
+
+
+    x = {
+        frozenset(["vision"]): {
+            "vision": torch.Tensor([1.0, 0.0, 0.3, ...]),
+        },
+        frozenset(["language"]): {
+            "language": torch.Tensor([1.0, 0.2, 0.9, ...]),
+        },
+        frozenset(["vision", "language"]): {
+            "vision": torch.Tensor([0.0, 1.0, 0.0, ...]),
+            "language": torch.Tensor([0.0, 0.3, 0.2, ...]),
+        },
+    }
+
+    fun(x)
+    ```
+Note:
+    This type uses `collections.abc.Mapping` and is used for functions' inputs.
+    Use `LatentsDomainGroupsDT` for functions' outputs.
+
+    This allows to be more generic and allow passing other mappings.
+    
 """
 
 LatentsDomainGroupsDT = dict[frozenset[str], LatentsDomainGroupDT]
@@ -43,23 +194,34 @@ Mapping of `LatentsDomainGroupDT`.
 Keys are frozenset of domains matched in the group.
 Each group is independent and contains different data (unpaired).
 
-This is a more specific version of `LatentsDomainGroupsT` used in method's outputs.
+Example:
+    ```python
+    def fun() -> LatentsDomainGroupsDT: 
+        return {
+            frozenset(["vision"]): {
+                "vision": torch.Tensor([1.0, 0.0, 0.3, ...]),
+            },
+            frozenset(["language"]): {
+                "language": torch.Tensor([1.0, 0.2, 0.9, ...]),
+            },
+            frozenset(["vision", "language"]): {
+                "vision": torch.Tensor([0.0, 1.0, 0.0, ...]),
+                "language": torch.Tensor([0.0, 0.3, 0.2, ...]),
+            },
+        }
+
+    ```
+
+Note:
+    This type uses `dict`s and is used for functions' outputs.
+    Use `LatentsDomainGroupT` for functions' inputs.
 """
 
-RawDomainGroupsT = Mapping[frozenset[str], RawDomainGroupT]
-"""
-Mapping of `RawDomainGroupT`. Keys are frozenset of domains matched in the group.
-Each group is independent and contains different data (unpaired).
-"""
-
-RawDomainGroupsDT = dict[frozenset[str], RawDomainGroupDT]
-"""
-Mapping of `RawDomainGroupT`. Keys are frozenset of domains matched in the group.
-Each group is independent and contains different data (unpaired).
-
-This is a more specific version of `RawDomainGroupsT` used in method's outputs.
-"""
 
 ModelModeT = Literal["train", "val", "test", "val/ood", "test/ood"]
-"""Mode used by pytorch lightning.
+"""
+Mode used by pytorch lightning (train/val, ...).
+
+When validating or testing in out-of-distribution data, "val/ood" or "test/ood" mode is
+used.
 """
