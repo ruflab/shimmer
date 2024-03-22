086b1b6151b3aca0f403267a36680db0db66e1ec
bdvllrs <bdvllrs@gmail.com>
2024-03-22 13:37:25 +0000

    Add version picker in main page

--- docs/make.py
+++ docs/make.py
@@ -2,6 +2,7 @@ import sys
 from pathlib import Path
 
 import pdoc
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 modules = [
     "shimmer.types",
@@ -26,3 +27,27 @@ if __name__ == "__main__":
 
     pdoc.render.configure(docformat="google", math=True)
     pdoc.pdoc(*modules, output_directory=here / "api" / version)
+
+    env = Environment(
+        loader=FileSystemLoader(here),
+        autoescape=select_autoescape(),
+        comment_start_string="{=",
+        comment_end_string="=}",
+    )
+    template = env.get_template("index.html.jinja2")
+
+    latest_version: None | str = None
+    doc_versions: list[str] = []
+    for version in (here / "api").iterdir():
+        if version.name == "index.html":
+            continue
+        if version.name != "latest":
+            doc_versions.append(version.name)
+        else:
+            latest_version = version.name
+    doc_versions = list(sorted(doc_versions, reverse=True))
+    if latest_version is not None:
+        doc_versions = [latest_version] + doc_versions
+
+    with open(here / "api" / "index.html", "w") as f:
+        f.write(template.render(versions=doc_versions))
