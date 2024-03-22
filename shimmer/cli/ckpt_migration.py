8fb150613c007a6fa4d193b652a58da03b12cee6
bdvllrs <bdvllrs@gmail.com>
2024-03-22 14:34:37 +0000

    Allow several paths to the checkpoint

--- shimmer/cli/ckpt_migration.py
+++ shimmer/cli/ckpt_migration.py
@@ -1,4 +1,4 @@
-import logging
+from collections.abc import Sequence
 from pathlib import Path
 
 import click
@@ -10,21 +10,22 @@ from migrate_ckpt import (
 
 from shimmer import MIGRATION_DIR
 
-logger = logging.getLogger("shimmer")
-
 
 @click.command("migrate-ckpt")
 @click.argument(
-    "path", type=click.Path(exists=True, path_type=Path), help="Path to the checkpoint"
+    "paths",
+    nargs=-1,
+    type=click.Path(exists=True, path_type=Path, file_okay=True, dir_okay=False),
 )
-def migrate_ckpt(path: Path):
-    ckpt = torch.load(path, map_location="cpu")
-    new_ckpt, done_migrations = migrate_from_folder(ckpt, MIGRATION_DIR)
-    done_migration_log = ", ".join(map(lambda x: x.name, done_migrations))
-    logger.info(f"Migrating: {done_migration_log}")
-    if len(done_migrations) or ckpt_migration_key not in ckpt:
-        version = 0
-        if ckpt_migration_key in ckpt:
-            version = len(ckpt[ckpt_migration_key])
-        torch.save(ckpt, path.with_stem(f"{path.stem}-{version}"))
-        torch.save(new_ckpt, path)
+def migrate_ckpt(paths: Sequence[Path]):
+    for path in paths:
+        ckpt = torch.load(path, map_location="cpu")
+        new_ckpt, done_migrations = migrate_from_folder(ckpt, MIGRATION_DIR)
+        done_migration_log = ", ".join(map(lambda x: x.name, done_migrations))
+        print(f"Migrating: {done_migration_log}")
+        if len(done_migrations) or ckpt_migration_key not in ckpt:
+            version = 0
+            if ckpt_migration_key in ckpt:
+                version = len(ckpt[ckpt_migration_key])
+            torch.save(ckpt, path.with_stem(f"{path.stem}-{version}"))
+            torch.save(new_ckpt, path)
