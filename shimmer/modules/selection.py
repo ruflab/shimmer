125c1bbfd59d76fab07c4a6748e652d77dfd86d9
bdvllrs <bdvllrs@gmail.com>
2024-03-27 14:18:38 +0000

    Add method to update the selection method's internal representation of the GW state Add docstring to selection.

--- shimmer/modules/selection.py
+++ shimmer/modules/selection.py
@@ -6,7 +6,42 @@ from shimmer.types import LatentsDomainGroupT
 
 
 class SelectionBase(torch.nn.Module, ABC):
+    """
+    This is the base class for the selection mechanism.
+    The selection mechanisms handles the "competition" between modules and *selects*
+    fusion coefficients for the domains.
+    """
+
+    def update_gw_state(self, gw_state: torch.Tensor) -> None:
+        """
+        Update the internal copy of the previous GW state.
+        By default, this is not implemented and will raise an error if used.
+
+        :note..
+            This is not defined as an abstractmethod as some Selection method may
+            not need it.
+
+        Args:
+            gw_state (`torch.Tensor`): the previous GW state
+        """
+        raise NotImplementedError()
+
     @abstractmethod
-    def forward(
-        self, domains: LatentsDomainGroupT, gw_state: torch.Tensor
-    ) -> dict[str, torch.Tensor]: ...
+    def forward(self, domains: LatentsDomainGroupT) -> dict[str, torch.Tensor]:
+        """
+        Forward pass of the selection method.
+
+        Args:
+            domains (`LatentsDomainGroupT`): Group of unimodal latent representations.
+
+        Returns:
+            `dict[str, torch.Tensor]`: for each domain in the group, the fusion
+            coefficient for each item in the batch.
+
+        Example:
+            >>> SomeSelectionImplementation().forward(
+            ...     {"v": torch.randn(3, 4), "t": torch.randn(3, 8)}
+            ... )
+            {"v": torch.Tensor([0.0, 0.4, 1.0]), "t": torch.Tensor([1.0, 0.6, 0.0])}
+        """
+        ...
