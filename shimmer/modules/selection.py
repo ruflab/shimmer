5899eaf6f2747a6f9eb7bfc595cb70d11bd66ef2
RolandBERTINJOHANNET <roland.bertin-johannet@cnrs.fr>
2024-03-27 15:16:10 +0000

    fixed rebase's code duplications

--- shimmer/modules/selection.py
+++ shimmer/modules/selection.py
@@ -7,7 +7,6 @@ from shimmer.types import LatentsDomainGroupT
 
 
 class SelectionBase(torch.nn.Module, ABC):
-<<<<<<< HEAD
     """
     This is the base class for the selection mechanism.
     The selection mechanisms handles the "competition" between modules and *selects*
@@ -86,78 +85,12 @@ class KQAttentionOnePass(SelectionBase):
         }
 
         return attention_dict
-=======
+
     @abstractmethod
     def forward(
         self, domains: LatentsDomainGroupT, gw_state: torch.Tensor
     ) -> dict[str, torch.Tensor]: ...
 
-
-<<<<<<< HEAD
-# todo : make this return a dict of attention scores. and move it into randomattention.
-def sample_scaling_factors(
-    binary_scaling_prob: float,
-    batch_size: int,
-    temperature: float,
-    device: torch.device,
-):
-    """
-    Args:
-        binary_scaling_prob (`float`): Should be between 0 and 1.
-        batch_size (`int`):
-        temperature (`float`): Should be greater than 0.
-        device (`torch.device`):
-    """
-    assert 0 <= binary_scaling_prob <= 1
-
-    def decode(
-        self, z: torch.Tensor, domains: Iterable[str] | None = None
-    ) -> LatentsDomainGroupDT:
-        """Decode the GW representation into given `domains`.
-
-        Args:
-            z (`torch.Tensor`): the GW representation.
-            domains (`Iterable[str]`): iterable of domains to decode.
-
-        Returns:
-            `LatentsDomainGroupDT`: the decoded unimodal representations.
-        """
-        ...
-
-    binary_mask = torch.rand(batch_size) < binary_scaling_prob
-
-    binary_factors = torch.randint(0, 2, (batch_size,)).float()
-    binary_softmax = torch.stack([binary_factors, 1 - binary_factors], dim=1)
-
-    uniform_samples = torch.rand(batch_size)
-    uniform_for_softmax = torch.stack([uniform_samples, 1 - uniform_samples], dim=1)
-
-    uniform_softmax = F.softmax(uniform_for_softmax * temperature, dim=1)
-
-    scaling_factors = torch.where(
-        binary_mask.unsqueeze(-1), binary_softmax, uniform_softmax
-    ).to(device)
-
-    binary_indices = torch.where(binary_mask)[0]
-    softmax_indices = torch.where(~binary_mask)[0]
-
-    binary_scaling_factors = scaling_factors[binary_indices]
-    softmax_scaling_factors = scaling_factors[softmax_indices]
-
-    return {
-        "binary": (
-            binary_scaling_factors[:, 0],
-            binary_scaling_factors[:, 1],
-            binary_indices,
-        ),
-        "softmax": (
-            softmax_scaling_factors[:, 0],
-            softmax_scaling_factors[:, 1],
-            softmax_indices,
-        ),
-    }
-
->>>>>>> 380f5f2 (I've no idea why rebase didn't create selection.py but I'm creating it here)
 
 class RandomSelection(SelectionBase):
     def __init__(self, binary_proportion, temperature):
@@ -165,15 +98,10 @@ class RandomSelection(SelectionBase):
         self.binary_proportion = binary_proportion
         self.temperature = temperature
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     def forward(
         self, domains: LatentsDomainGroupT, gw_states: torch.Tensor
     ) -> dict[str, torch.Tensor]:
         pass
-=======
-    def forward(self, domains: LatentsDomainGroupT, gw_states: torch.Tensor) -> dict[str, float]:
-        return sample_scaling_factors(self.binary_proportion, gw_states.shape[0], self.temperature, gw_states.device)
 
 
 
@@ -185,23 +113,7 @@ class RandomSelection(SelectionBase):
 
 
 
-=======
-    def forward(
-        self, domains: LatentsDomainGroupT, gw_states: torch.Tensor
-    ) -> dict[str, float]:
-        return sample_scaling_factors(
-            self.binary_proportion,
-            gw_states.shape[0],
-            self.temperature,
-            gw_states.device,
-        )
->>>>>>> 8fdbc34 (trying ruff format again..)
-
-
-class KQAttentionOnePass(nn.Module):
-=======
 class KQAttentionOnePass(SelectionBase):
->>>>>>> c95c4ed (took out BinaryAttention -no longer needed)
     def __init__(self, domain_dim, head_size):
         super().__init__()
         self.head_size = head_size
@@ -240,29 +152,3 @@ class KQAttentionOnePass(SelectionBase):
         return attention_dict
 
 
-class RandomSelection(SelectionBase):
-    def __init__(self, binary_proportion, temperature):
-        super().__init__()
-        self.binary_proportion = binary_proportion
-        self.temperature = temperature
-
-<<<<<<< HEAD
-        weights = {}
-
-        # unsure what is meant by "1 if domain is here, 0 otherwise" -- how do we get the domain key if it's not there ?
-        def forward(self, domain_encodings: LatentsDomainGroupT) -> LatentsDomainGroupT:
-            for domain, encoding in domain_encodings.items():
-                weights[domain] = torch.ones(encoding.shape[0])
-
-            return weights
-<<<<<<< HEAD
-
->>>>>>> 380f5f2 (I've no idea why rebase didn't create selection.py but I'm creating it here)
-=======
->>>>>>> 8fdbc34 (trying ruff format again..)
-=======
-    def forward(
-        self, domains: LatentsDomainGroupT, gw_states: torch.Tensor
-    ) -> dict[str, torch.Tensor]:
-        pass
->>>>>>> c95c4ed (took out BinaryAttention -no longer needed)
