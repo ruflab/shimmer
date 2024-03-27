8597d7c614e9d7c7c5f132e3594063d89e312fbd
bdvllrs <bdvllrs@gmail.com>
2024-03-27 10:07:08 +0000

    Add SingleDomainSelection Module

--- shimmer/modules/selection.py
+++ shimmer/modules/selection.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 import torch
 
 from shimmer.types import LatentsDomainGroupT
+from shimmer.utils import group_batch_size, group_device
 
 
 class SelectionBase(torch.nn.Module, ABC):
@@ -10,3 +11,34 @@ class SelectionBase(torch.nn.Module, ABC):
     def forward(
         self, domains: LatentsDomainGroupT, gw_state: torch.Tensor
     ) -> dict[str, torch.Tensor]: ...
+
+
+class SingleDomainSelection(SelectionBase):
+    """
+    This selection mechanism handles groups that can have multiple domains, but always
+    return a selection of 1 domain from the group with a uniform distribution.
+
+    For example, if the group has 2 domains, there is a 50% chance of selecting each
+    domain.
+    """
+
+    def forward(
+        self, domains: LatentsDomainGroupT, gw_state: torch.Tensor
+    ) -> dict[str, torch.Tensor]:
+        """
+        Forward pass of the module.
+
+        Args:
+            domains (`LatentsDomainGroupT`): input unimodal latent representations
+            gw_state (`torch.Tensor`): the previous GW state
+
+        Returns:
+            `dict[str, torch.Tensor]`: whether the domain is selected for each input
+            in the batch.
+        """
+        selection: dict[str, torch.Tensor] = {}
+        bs = group_batch_size(domains)
+        choice = torch.randint(len(domains), size=(bs,), device=group_device(domains))
+        for k, domain in enumerate(domains.keys()):
+            selection[domain] = (choice == k).to(torch.float32)
+        return selection
---
+++ tests/test_single_domain_selection.py
@@ -0,0 +1,51 @@
+import torch
+
+from shimmer.modules.selection import SingleDomainSelection
+
+
+def test_selection_1_domain():
+    selection_mod = SingleDomainSelection()
+
+    bs = 32
+    domains = {"v": torch.randn(bs, 8)}
+    gw_state = torch.Tensor(0)  # Not really useful. Can be anything.
+
+    selection: dict[str, torch.Tensor] = selection_mod(domains, gw_state)
+
+    assert len(selection) == len(domains)
+    assert next(iter(selection.keys())) == "v"
+    assert (selection["v"] == 0).sum() == 0, "Everything should be selected."
+
+
+def test_selection_2_domains():
+    selection_mod = SingleDomainSelection()
+
+    bs = 32
+    domains = {"v": torch.randn(bs, 8), "t": torch.randn(bs, 12)}
+    gw_state = torch.Tensor(0)
+
+    selection: dict[str, torch.Tensor] = selection_mod(domains, gw_state)
+
+    assert len(selection) == len(domains)
+    assert (
+        (selection["v"] + selection["t"]) == 1
+    ).sum() == bs, "Everything should be selected once and only once."
+
+
+def test_selection_3_domains():
+    selection_mod = SingleDomainSelection()
+
+    bs = 32
+    domains = {
+        "v": torch.randn(bs, 8),
+        "t": torch.randn(bs, 12),
+        "attr": torch.randn(bs, 4),
+    }
+    gw_state = torch.Tensor(0)
+
+    selection: dict[str, torch.Tensor] = selection_mod(domains, gw_state)
+
+    assert len(selection) == len(domains)
+    assert (
+        (selection["v"] + selection["t"] + selection["attr"]) == 1
+    ).sum() == bs, "Everything should be selected once and only once."
