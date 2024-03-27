0272296da8c00ef037fdd830628d209b16035322
Lara <lara@scipio.nl>
2024-03-27 15:53:27 +0100

    Structure for query attention

--- shimmer/modules/selection.py
+++ shimmer/modules/selection.py
@@ -227,3 +227,192 @@ class RandomSelection(SelectionBase):
             domain: all_scores[:, i : i + 1] for i, domain in enumerate(domains)
         }
         return attention_dict
+
+
+# todo : make this return a dict of attention scores. and move it into randomattention.
+# def sample_scaling_factors(
+#     binary_scaling_prob: float,
+#     batch_size: int,
+#     temperature: float,
+#     device: torch.device,
+# ):
+#     """
+#     Args:
+#         binary_scaling_prob (`float`): Should be between 0 and 1.
+#         batch_size (`int`):
+#         temperature (`float`): Should be greater than 0.
+#         device (`torch.device`):
+#     """
+#     assert 0 <= binary_scaling_prob <= 1
+
+#     def decode(
+#         self, z: torch.Tensor, domains: Iterable[str] | None = None
+#     ) -> LatentsDomainGroupDT:
+#         """Decode the GW representation into given `domains`.
+
+#         Args:
+#             z (`torch.Tensor`): the GW representation.
+#             domains (`Iterable[str]`): iterable of domains to decode.
+
+#         Returns:
+#             `LatentsDomainGroupDT`: the decoded unimodal representations.
+#         """
+#         ...
+
+#     binary_mask = torch.rand(batch_size) < binary_scaling_prob
+
+#     binary_factors = torch.randint(0, 2, (batch_size,)).float()
+#     binary_softmax = torch.stack([binary_factors, 1 - binary_factors], dim=1)
+
+#     uniform_samples = torch.rand(batch_size)
+#     uniform_for_softmax = torch.stack([uniform_samples, 1 - uniform_samples], dim=1)
+
+#     uniform_softmax = F.softmax(uniform_for_softmax * temperature, dim=1)
+
+#     scaling_factors = torch.where(
+#         binary_mask.unsqueeze(-1), binary_softmax, uniform_softmax
+#     ).to(device)
+
+#     binary_indices = torch.where(binary_mask)[0]
+#     softmax_indices = torch.where(~binary_mask)[0]
+
+#     binary_scaling_factors = scaling_factors[binary_indices]
+#     softmax_scaling_factors = scaling_factors[softmax_indices]
+
+#     return {
+#         "binary": (
+#             binary_scaling_factors[:, 0],
+#             binary_scaling_factors[:, 1],
+#             binary_indices,
+#         ),
+#         "softmax": (
+#             softmax_scaling_factors[:, 0],
+#             softmax_scaling_factors[:, 1],
+#             softmax_indices,
+#         ),
+#     }
+
+
+# class RandomSelection(SelectionBase):
+#     def __init__(self, binary_proportion, temperature):
+#         super().__init__()
+#         self.binary_proportion = binary_proportion
+#         self.temperature = temperature
+
+#     def forward(
+#         self, domains: LatentsDomainGroupT, gw_states: torch.Tensor
+#     ) -> dict[str, float]:
+#         return sample_scaling_factors(
+#             self.binary_proportion,
+#             gw_states.shape[0],
+#             self.temperature,
+#             gw_states.device,
+#         )
+
+
+class KQAttentionOnePass(nn.Module):
+    def __init__(self, domain_dim, head_size):
+        super().__init__()
+        self.head_size = head_size
+        self.query_layer = nn.Linear(domain_dim, head_size)
+        self.key_layers = nn.ModuleDict(
+            {
+                "v_latents": nn.Linear(domain_dim, head_size),
+                "attr": nn.Linear(domain_dim, head_size),
+            }
+        )
+
+    def forward(
+        self, domains: dict[str, torch.Tensor], gw_state: torch.Tensor
+    ) -> dict[str, torch.Tensor]:
+        keys = {
+            domain: self.key_layers[domain](encoding)
+            for domain, encoding in domains.items()
+        }
+
+        device = gw_state.device
+        query = self.query_layer(gw_state.to(device))
+
+        dot_products = {
+            domain: torch.bmm(key.unsqueeze(1), query.unsqueeze(2)).squeeze()
+            for domain, key in keys.items()
+        }
+
+        dot_products_tensor = torch.stack(list(dot_products.values()), dim=1)
+
+        attention_scores = torch.softmax(dot_products_tensor, dim=1)
+
+        attention_dict = {
+            domain: attention_scores[:, i : i + 1] for i, domain in enumerate(keys)
+        }
+
+        return attention_dict
+
+
+class BinaryAttention(SelectionBase):
+    def __init__(self, domain_dim, head_size):
+        super().__init__()
+
+        weights = {}
+
+        # unsure what is meant by "1 if domain is here, 0 otherwise" -- how do we get the domain key if it's not there ?
+        def forward(self, domain_encodings: LatentsDomainGroupT) -> LatentsDomainGroupT:
+            for domain, encoding in domain_encodings.items():
+                weights[domain] = torch.ones(encoding.shape[0])
+
+            return weights
+
+
+class QueryAttention(SelectionBase):
+    def __init__(self, domain_dim, head_size):
+        """
+        Args:
+            domain_dim (`int`): the dimension of the domain encodings.
+            head_size (`int`): the size of the attention heads.
+        """
+        super().__init__()
+        self.head_size = head_size
+        self.query_layer = nn.Linear(
+            domain_dim, head_size
+        )  # This is the naive query to start with
+        self.key_layers = nn.ModuleDict(
+            {
+                "v_latents": nn.Linear(domain_dim, head_size),
+                "attr": nn.Linear(domain_dim, head_size),
+            }
+        )
+
+    def forward(
+        self, domains: dict[str, torch.Tensor], gw_state: torch.Tensor
+    ) -> dict[str, torch.Tensor]:
+        """
+        Args:
+            domains (`Dict[str, torch.Tensor]`): the domain encodings.
+            gw_state (`torch.Tensor`): the global workspace state.
+        """
+        # to be changed
+
+        keys = {
+            domain: self.key_layers[domain](encoding)
+            for domain, encoding in domains.items()
+        }
+
+        device = gw_state.device
+
+        # The query should be the global workspace state
+        query = self.query_layer(gw_state.to(device))
+
+        dot_products = {
+            domain: torch.bmm(key.unsqueeze(1), query.unsqueeze(2)).squeeze()
+            for domain, key in keys.items()
+        }
+
+        dot_products_tensor = torch.stack(list(dot_products.values()), dim=1)
+
+        attention_scores = torch.softmax(dot_products_tensor, dim=1)
+
+        attention_dict = {
+            domain: attention_scores[:, i : i + 1] for i, domain in enumerate(keys)
+        }
+
+        return attention_dict
