4ebb3b4ab3903ec3d30a2d26f89f1be00201c41a
Lara <lara@scipio.nl>
2024-04-23 12:10:34 +0200

    changes to attention module

--- shimmer/modules/attention_module.py
+++ shimmer/modules/attention_module.py
@@ -1,10 +1,22 @@
-from collections.abc import Callable, Mapping
+import random
+from collections.abc import Callable, Mapping, Sequence
 from typing import Any
 
 import torch
 from lightning.pytorch import LightningModule
-from modules.selection import DynamicQueryAttention
+from lightning.pytorch.utilities.types import OptimizerLRSchedulerConfig
+from modules.selection import DynamicQueryAttention, SelectionBase
 from torch import Tensor, nn
+from torch.optim.lr_scheduler import OneCycleLR
+
+from shimmer.modules.global_workspace import GlobalWorkspaceBase, SchedulerArgs
+from shimmer.modules.gw_module import GWModuleBase
+from shimmer.modules.losses import GWLossesBase
+from shimmer.types import (
+    LatentsDomainGroupsDT,
+    LatentsDomainGroupsT,
+    RawDomainGroupsT,
+)
 
 
 class ClassificationHead(nn.Module):
@@ -44,12 +56,14 @@ class DynamicAttention(LightningModule):
 
     def __init__(
         self,
-        gw_module: nn.Module,
+        gw_module: GlobalWorkspaceBase[GWModuleBase, SelectionBase, GWLossesBase],
         batch_size: int,
         domain_dim: int,
         head_size: int,
-        domain_names: list[str],
-        criterion: Callable[[torch.Tensor, torch.Tensor], torch.Tensor],
+        domain_names: Sequence[str],
+        criterion: Callable[[torch.Tensor, RawDomainGroupsT], torch.Tensor],
+        optim_lr: float,
+        scheduler_args: SchedulerArgs | None = None,
     ):
         super().__init__()
         self.save_hyperparameters(ignore=["criterion"])
@@ -60,17 +74,42 @@ class DynamicAttention(LightningModule):
         )
         self.domain_names = domain_names
         self.criterion = criterion
-        self.optimizer = torch.optim.Adam(list(self.attention.parameters()), lr=0.0005)
+        self.optim_lr = optim_lr
+        self.scheduler_args = SchedulerArgs(max_lr=optim_lr, total_steps=1)
+
+    def configure_optimizers(self) -> OptimizerLRSchedulerConfig:
+        """
+        Configure models optimizers.
+
+        Here we use `AdamW` for the optimizer and `OneCycleLR` for the learning-rate
+        scheduler.
+        """
+
+        optimizer = torch.optim.AdamW(
+            self.parameters(),
+            lr=self.optim_lr,
+            weight_decay=self.optim_weight_decay,
+        )
+
+        lr_scheduler = OneCycleLR(optimizer, **self.scheduler_args)
+
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": {
+                "scheduler": lr_scheduler,
+                "interval": "step",
+            },
+        }
 
     def forward(self, single_domain_input, prefusion_encodings):
         return self.attention(single_domain_input, prefusion_encodings)
 
     def apply_corruption(
-        batch: dict,
-        dict_domain_names: frozenset,
-        corruption_vector=None,
-        corrupted_domain=None,
-    ):
+        self,
+        batch: LatentsDomainGroupsT,
+        corruption_vector: torch.Tensor | None = None,
+        corrupted_domain: str | None = None,
+    ) -> LatentsDomainGroupsT:
         """
         Apply corruption to the batch.
 
@@ -80,54 +119,61 @@ class DynamicAttention(LightningModule):
             gw_dim: The global workspace dimension.
             corruption_vector: The corruption vector, if defined outside training.
         """
-
-        corrupted_domain = "attr"
         if corrupted_domain is None:
             # Specify which domain will be corrupted
-            corrupted_domain = random.choice(list(dict_domain_names))
+            corrupted_domain = random.choice(self.domain_names)
 
-        # Get the data tensor with only matched data
-        matched_data_dict = batch[dict_domain_names]
+        matched_data_dict: LatentsDomainGroupsDT = {}
+        for domain_names, domains in batch.items():
+            for domain_name, domain in domains.items():
+                if domain_names != self.domain_names or domain_name != corrupted_domain:
+                    matched_data_dict.setdefault(domain_names, {})[domain_name] = domain
+                    continue
 
-        # Remove the list around value of 'attr'
-        for key, _ in matched_data_dict.items():
-            if key == "attr":
-                matched_data_dict[key] = matched_data_dict[key][0]
+                # If corruption vector is not fixed outside the loop
+                if corruption_vector is None:
+                    corruption_vector = torch.randn_like(domain)
 
-        tensor_size = matched_data_dict[corrupted_domain].shape
-
-        # If corruption vector is not fixed outside the loop
-        if corruption_vector is None:
-            corruption_vector = torch.randn(tensor_size, device=torch.device("cuda:0"))
-
-        # Some tensors have type long, some type float
-        matched_data_dict[corrupted_domain] = matched_data_dict[corrupted_domain].to(
-            torch.float32
-        )
-        corruption_vector = corruption_vector.to(
-            matched_data_dict[corrupted_domain].device
-        )
-
-        # Apply element-wise addition to one of the domains
-        matched_data_dict[corrupted_domain] += corruption_vector
+                # Apply element-wise addition to one of the domains
+                matched_data_dict.setdefault(domain_names, {})[domain_name] = (
+                    domain + corruption_vector
+                )
 
         return matched_data_dict
 
-    def training_step(self, batch, batch_idx) -> Tensor | Mapping[str, Any] | None:
-        # Assume its one batch
-        filtered_batch = batch[0]
-        # How to get the shape info?
-        target = shape_info_attr
-        corrupted_batch = self.apply_corruption(filtered_batch, self.domain_names)
+    def generic_step(
+        self, batch: RawDomainGroupsT, batch_idx: int
+    ) -> Tensor | Mapping[str, Any] | None:
+        latent_domains = self.gw_module.encode_domains(batch)
+
+        corrupted_batch = self.apply_corruption(latent_domains)
+
+        prefusion_encodings = self.gw_module.encode(corrupted_batch)
         attention_scores = self.forward(corrupted_batch, prefusion_encodings)
-        merged_gw_representation = module.encode_and_fuse(
-            attention_scores, self.attention
+        merged_gw_representation = self.gw_module.gw_mod.fuse(
+            prefusion_encodings, attention_scores
         )
 
-        loss = self.criterion(merged_gw_representation, target)
+        loss = self.criterion(merged_gw_representation, batch)
+
+        for name, metric in loss.all.items():
+            self.log(
+                f"{name}",
+                metric,
+                batch_size=self.batch_size,
+                add_dataloader_idx=False,
+            )
         return loss
+
+    def training_step(
+        self, batch: RawDomainGroupsT, batch_idx: int
+    ) -> Tensor | Mapping[str, Any] | None:  # type: ignore
+        return self.generic_step(batch, batch_idx)
 
     def validation_step(
         self, *args: Any, **kwargs: Any
     ) -> Tensor | Mapping[str, Any] | None:
         return super().validation_step(*args, **kwargs)
+
+    def test_step(self, *args: Any, **kwargs: Any) -> Tensor | Mapping[str, Any] | None:
+        return super().test_step(*args, **kwargs)
--- shimmer/utils.py
+++ shimmer/utils.py
@@ -37,6 +37,22 @@ def groups_batch_size(domain_latents: LatentsDomainGroupsT) -> int:
     raise ValueError("Empty batch.")
 
 
+def groups_device(domain_latents: LatentsDomainGroupsT) -> int:
+    """
+    Get the batch size of the batch.
+
+    Args:
+        domain_latents (`LatentsDomainGroupsT`): the batch of groups.
+
+    Returns:
+        int: the batch size.
+    """
+    for data in domain_latents.values():
+        for tensor in data.values():
+            return tensor.device
+    raise ValueError("Empty batch.")
+
+
 def group_device(x: LatentsDomainGroupT) -> torch.device:
     for val in x.values():
         return val.device
