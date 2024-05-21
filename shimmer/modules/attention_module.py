9037ffd4dabc6169ed88c549e75349a3d1fe6781
larascipio <lara@scipio.nl>
2024-05-21 08:17:58 +0000

    changed shape classifier to the new classifier

--- shimmer/modules/attention_module.py
+++ shimmer/modules/attention_module.py
@@ -25,31 +25,28 @@ from shimmer.types import (
 )
 
 
-class ShapesClassifier(nn.Module):
+class ShapeClassifier(nn.Sequential):
     def __init__(self, input_dim, output_dim):
-        super().__init__()
-        self.fc1 = nn.Linear(input_dim, 256)
-        self.bn1 = nn.BatchNorm1d(256)
-        self.fc2 = nn.Linear(256, 128)
-        self.bn2 = nn.BatchNorm1d(128)
-        self.fc3 = nn.Linear(128, 64)
-        self.bn3 = nn.BatchNorm1d(64)
-        self.fc4 = nn.Linear(64, 32)
-        self.bn4 = nn.BatchNorm1d(32)
-        self.fc5 = nn.Linear(32, output_dim)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(p=0.5)
-
-    def forward(self, x):
-        x = self.relu(self.bn1(self.fc1(x)))
-        x = self.dropout(x)
-        x = self.relu(self.bn2(self.fc2(x)))
-        x = self.dropout(x)
-        x = self.relu(self.bn3(self.fc3(x)))
-        x = self.dropout(x)
-        x = self.relu(self.bn4(self.fc4(x)))
-        x = self.dropout(x)
-        return self.fc5(x)
+        layers = [
+            nn.Linear(input_dim, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(256, 128),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(128, 64),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(64, 32),
+            nn.BatchNorm1d(32),
+            nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(32, output_dim),
+        ]
+        super().__init__(*layers)
 
 
 class AttentionBase(LightningModule):
@@ -173,7 +170,7 @@ class AttentionBase(LightningModule):
         accuracies = []
 
         for domain_names, domains in merged_gw_representation.items():
-            loss, accuracy = (self.criterion(domains, batch[domain_names]))
+            loss, accuracy = self.criterion(domains, batch[domain_names])
             losses.append(loss)
             accuracies.append(accuracy)
             domain_names_str = ",".join(domain_names)
