668e1661f3c7f6d931e52d757bed7808bbb87406
larascipio <lara@scipio.nl>
2024-05-27 10:31:18 +0000

    add other prints

--- shimmer/modules/attention_module.py
+++ shimmer/modules/attention_module.py
@@ -136,16 +136,18 @@ class AttentionBase(LightningModule):
                 if self.fixed_corruption_vector is None:
                     print("3")
                     corruption_vector = torch.randn(domain.size(1)).to("cuda:0")
-                elif self.fixed_corruption_vector.shape != domain.shape:
-                    print("2")
-                    print(self.fixed_corruption_vector.shape)
-                    print(domain.shape)
-                    corruption_vector = self.fixed_corruption_vector[: domain.shape[0]]
-                    print(corruption_vector.shape)
-                    print(corruption_vector)
+                # elif self.fixed_corruption_vector.shape != domain.shape:
+                #     print("2")
+                #     print(self.fixed_corruption_vector.shape)
+                #     print(domain.shape)
+                #     corruption_vector = self.fixed_corruption_vector[: domain.shape[0]]
+                #     print(corruption_vector.shape)
+                #     print(corruption_vector)
                 else:
                     print("1")
                     corruption_vector = self.fixed_corruption_vector
+                    print(corruption_vector.shape)
+                    print(corruption_vector)
 
                 normalized_corruption_vector = (
                     corruption_vector - corruption_vector.mean()
