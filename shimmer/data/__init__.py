835a2846f70d36dd9e9fbb76c714ca0a4044f2ea
bdvllrs <bdvllrs@gmail.com>
2024-06-04 09:59:20 +0000

    Add classes to __init__

---
+++ shimmer/data/__init__.py
@@ -0,0 +1,9 @@
+from shimmer.data.dataset import DomainType, RepeatedDataset, ShimmerDataset
+from shimmer.data.domain import DataDomain
+
+__all__ = [
+    "DomainType",
+    "RepeatedDataset",
+    "ShimmerDataset",
+    "DataDomain",
+]
