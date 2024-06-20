227f9bc7e06c307013dcaaa3b1cc645f58d2e371
bdvllrs <bdvllrs@gmail.com>
2024-06-20 10:32:24 +0200

    Add test of checkpoint migrations (#87)
    * add test script to save a gw
    
    * Add test for migrating an old checkpoint
    
    * Add information on how to contribute a checkpoint migration

---
+++ tests/test_ckpt_migrations.py
@@ -0,0 +1,132 @@
+from pathlib import Path
+
+import torch
+import torch.utils.data
+from migrate_ckpt import migrate_from_folder
+from utils import DummyDomainModule
+
+from shimmer import GlobalWorkspace2Domains, GWDecoder, GWEncoder
+from shimmer.modules.global_workspace import GlobalWorkspace
+from shimmer.utils import MIGRATION_DIR
+
+here = Path(__file__).parent
+
+
+def test_ckpt_migration_2_domains():
+    old_ckpt_path = here / "data" / "old_gw_2_domains.ckpt"
+    old_ckpt = torch.load(old_ckpt_path)
+    new_ckpt, done_migrations = migrate_from_folder(old_ckpt, MIGRATION_DIR)
+
+    old_keys = set(old_ckpt["state_dict"].keys())
+    new_keys = set(new_ckpt["state_dict"].keys())
+    print(f"Removed keys: {old_keys - new_keys}")
+    print(f"New keys: {new_keys - old_keys}")
+
+    print("Done migrations:", ", ".join(map(lambda x: x.name, done_migrations)))
+
+    domains = {
+        "v": DummyDomainModule(latent_dim=32),
+        "t": DummyDomainModule(latent_dim=32),
+    }
+
+    workspace_dim = 16
+
+    gw_encoders = {
+        "v": GWEncoder(
+            domains["v"].latent_dim,
+            hidden_dim=64,
+            out_dim=workspace_dim,
+            n_layers=1,
+        ),
+        "t": GWEncoder(
+            domains["t"].latent_dim,
+            hidden_dim=64,
+            out_dim=workspace_dim,
+            n_layers=1,
+        ),
+    }
+
+    gw_decoders = {
+        "v": GWDecoder(
+            workspace_dim,
+            hidden_dim=64,
+            out_dim=domains["v"].latent_dim,
+            n_layers=1,
+        ),
+        "t": GWDecoder(
+            workspace_dim,
+            hidden_dim=64,
+            out_dim=domains["t"].latent_dim,
+            n_layers=1,
+        ),
+    }
+
+    gw = GlobalWorkspace2Domains(
+        domains,
+        gw_encoders,
+        gw_decoders,
+        workspace_dim=16,
+        loss_coefs={},
+    )
+
+    gw.load_state_dict(new_ckpt["state_dict"])
+
+
+def test_ckpt_migration_gw():
+    old_ckpt_path = here / "data" / "old_gw.ckpt"
+    old_ckpt = torch.load(old_ckpt_path)
+    new_ckpt, done_migrations = migrate_from_folder(old_ckpt, MIGRATION_DIR)
+
+    old_keys = set(old_ckpt["state_dict"].keys())
+    new_keys = set(new_ckpt["state_dict"].keys())
+    print(f"Removed keys: {old_keys - new_keys}")
+    print(f"New keys: {new_keys - old_keys}")
+
+    print("Done migrations:", ", ".join(map(lambda x: x.name, done_migrations)))
+
+    domains = {
+        "v": DummyDomainModule(latent_dim=32),
+        "t": DummyDomainModule(latent_dim=32),
+    }
+
+    workspace_dim = 16
+
+    gw_encoders = {
+        "v": GWEncoder(
+            domains["v"].latent_dim,
+            hidden_dim=64,
+            out_dim=workspace_dim,
+            n_layers=1,
+        ),
+        "t": GWEncoder(
+            domains["t"].latent_dim,
+            hidden_dim=64,
+            out_dim=workspace_dim,
+            n_layers=1,
+        ),
+    }
+
+    gw_decoders = {
+        "v": GWDecoder(
+            workspace_dim,
+            hidden_dim=64,
+            out_dim=domains["v"].latent_dim,
+            n_layers=1,
+        ),
+        "t": GWDecoder(
+            workspace_dim,
+            hidden_dim=64,
+            out_dim=domains["t"].latent_dim,
+            n_layers=1,
+        ),
+    }
+
+    gw = GlobalWorkspace(
+        domains,
+        gw_encoders,
+        gw_decoders,
+        workspace_dim=16,
+        loss_coefs={},
+    )
+
+    gw.load_state_dict(new_ckpt["state_dict"])
